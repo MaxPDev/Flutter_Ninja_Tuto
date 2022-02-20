1cbb3aa1ae478a8904af968eea98453f16dc2401
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2022-02-20 18:11:42 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/fr_piscadev_ninja_id/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fr_piscadev_ninja_id
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: NinjaCard(),
+  ));
+}
+
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ninja ID Card'),
+      ),
+    );
+  }
+}
