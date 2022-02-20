e43aafbbc6d8c959103cb4044baaad2e4825d950
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2022-02-20 21:15:31 +0100

    ninja id done

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,16 @@ class NinjaCard extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/chunli.jpeg'),
+                  radius: 40.0,
+                ),
+              ),
+              Divider(
+                height: 90.0,
+                color: Colors.grey[800],
+              ),
               Text(
                 'NAME',
                 style: TextStyle(
